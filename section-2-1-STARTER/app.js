bf015eb3eddd1eb6ccf3e99b83db07ecfd374541
PedroCantu <pedro.cantu_dev@outlook.com>
2024-10-02 14:02:07 -0600

    undestanding data in the vue instances

--- section-2-1-STARTER/app.js
+++ section-2-1-STARTER/app.js
@@ -1,5 +1,15 @@
+
+const lastNameOut = 'Doess';
+
 //We cereate a new vue app
 //by calling the createApp method
 // Passing an object to configure how the 
 //app will interact with the document
-Vue.createApp({}).mount('#app');
+Vue.createApp({
+    data(){
+        return {
+            firstName: 'John',
+            lastName: 'Doe'
+        }
+    }
+}).mount('#app');
